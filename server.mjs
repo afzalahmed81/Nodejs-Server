5fa95ea00e99f6776130d13c01f0c4df42bab4c0
afzalahmed81 <afzalahmed123@gmail.com>
2022-08-04 19:18:13 +0500

    added some libraries

--- server.mjs
+++ server.mjs
@@ -1,14 +1,28 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import express from 'express'
+import cors from 'cors';
 
-app.get('/home', (res,req) => {
-    res.send('This is home page!')
+const app = express();
+app.use(cors());
+
+app.get('/home', (req, res) => {
+  res.send('I am home page!')
 })
-app.get('/profile', (res,req) => {
-    res.send('This is profile page!')
+
+app.get('/profile', (req, res) => {
+  res.send('I am profile page!')
 })
+app.get('/weather', (req, res) => {
+
+  res.send({
+    weather: 'sunny',
+    temperature: '20',
+    city: 'London'
+  })
+
+})
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
